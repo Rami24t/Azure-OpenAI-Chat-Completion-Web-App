47c0eeea0c1a091dd82133376206c0587921be90
Rami <alsaadi.rami24@gmail.com>
2023-11-19 01:25:27 +0100

    use Context Provider

--- azure-open-ai-chat-completion/src/index.js
+++ azure-open-ai-chat-completion/src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import ContextProvider from "./components/Context";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>
 );
