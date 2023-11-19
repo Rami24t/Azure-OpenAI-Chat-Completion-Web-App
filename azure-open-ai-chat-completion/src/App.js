6e9f7cc3cdf2117c49be71dbdf48fab98a503aad
Rami <alsaadi.rami24@gmail.com>
2023-11-19 01:27:53 +0100

    develop UI and logic using Context & sim-response

--- azure-open-ai-chat-completion/src/App.js
+++ azure-open-ai-chat-completion/src/App.js
@@ -1,16 +1,41 @@
-import Inputs from "./components/Inputs";
-import ChatMessages from "./components/ChatMessages";
-import "./App.css";
+import { useContext } from "react";
+import { Context } from "./components/Context";
 import { PhoneHeader } from "./components/PhoneHeader";
 import { Paragraph } from "./components/Paragraph";
+import ChatMessages from "./components/ChatMessages";
+import Inputs from "./components/Inputs";
 import { Footer } from "./components/Footer";
+import { RESPONSE_SIMULATION } from "./RESPONSE_SIMULATION";
+import "./App.css";
 
 function App() {
-  
+  const { dispatch } = useContext(Context);
+
   async function sendQuery(systemMessage, userPrompt) {
-    setTimeout(() => {
-      alert(`sendQuery \n${systemMessage} \n${userPrompt}`);
-    }, 500);
+    if (!userPrompt) return;
+    console.log(`sendQuery \n${systemMessage} \n${userPrompt}`);
+
+    dispatch({
+      type: "SEND_MESSAGE_SUCCESS",
+      payload: {
+        text: userPrompt,
+        type: "sent",
+        date: new Date(),
+      },
+    });
+    const response = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(RESPONSE_SIMULATION);
+      }, 700);
+    });
+    dispatch({
+      type: "RECEIVE_MESSAGE_SUCCESS",
+      payload: {
+        text: response.choices[0].message.content,
+        type: "received",
+        date: new Date(response.created * 1000),
+      },
+    });
   }
 
   return (
--- azure-open-ai-chat-completion/src/components/ChatMessages.js
+++ azure-open-ai-chat-completion/src/components/ChatMessages.js
@@ -1,28 +1,63 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { Context } from "./Context";
 
 const ChatMessages = () => {
+  const { state } = useContext(Context);
+  const { chatHistory } = state;
+
   return (
-    <div>              {/* <div className="grey-chat">
+    <div className="chat-messages">
+      {chatHistory.map((message, index) => {
+        return (
+          <p
+            key={index}
+            className={`${
+              message.type === "sent" ? "white-chat sent" : "grey-chat received"
+            }`}
+          >
+            {message.text}
+          </p>
+        );
+      })}
+      {/* <p className="grey-chat received">
+        Yes, other Azure AI services also support customer managed keys. Azure
+        AI services offer multiple options for customers to manage keys, such as
+        using Azure Key Vault, customer-managed keys in Azure Key Vault or
+        customer-managed keys through Azure Storage service. This helps
+        customers ensure that their data is secure and access to their services
+        is controlled.
+      </p>
+      <p className="white-chat sent">
+        Does Azure OpenAI support customer managed keys?
+      </p> */}
+      {/* <div className="grey-chat">
+        <p className="typing-dots">
+          <div className="typing typing-1"></div>
+          <div className="typing typing-2"></div>
+          <div className="typing typing-3"></div>
+        </p>
+      </div> */}
+      {/* <div className="grey-chat">
     <div className="typing-dots">
       <div className="typing typing-1"></div>
       <div className="typing typing-2"></div>
       <div className="typing typing-3"></div>
     </div>
   </div> */}
-  {/* <p className="grey-chat received">
+      {/* <p className="grey-chat received">
     That sounds great. I’d be happy with that.
   </p> */}
-  {/* <div className="grey-chat">
+      {/* <div className="grey-chat">
     <div className="typing-dots">
       <div className="typing typing-1"></div>
       <div className="typing typing-2"></div>
       <div className="typing typing-3"></div>
     </div>
   </div> */}
-  {/* <p className="grey-chat received">
+      {/* <p className="grey-chat received">
     Could you send over some pictures of your dog, please?
   </p> */}
-  {/* <div className="white-chat">
+      {/* <div className="white-chat">
     <p>
       <p className="typing-dots">
         <div className="typing typing-1"></div>
@@ -31,12 +66,12 @@ const ChatMessages = () => {
       </p>
     </p>
   </div> */}
-  {/* <div className="photos sent">
+      {/* <div className="photos sent">
     <img src="https://source.unsplash.com/random/50x50?dog 1" alt="Dog 1" />
     <img src="https://source.unsplash.com/random/50x50?dog 2" alt="Dog 2" />
     <img src="https://source.unsplash.com/random/50x50?dog 3" alt="Dog 3" />
   </div> */}
-  {/* <div className="white-chat">
+      {/* <div className="white-chat">
     <p>
       <p className="typing-dots">
         <div className="typing typing-1"></div>
@@ -45,7 +80,7 @@ const ChatMessages = () => {
       </p>
     </p>
   </div> */}
-  {/* <div className="white-chat">
+      {/* <div className="white-chat">
     <p>
       <div className="typing-dots">
         <div className="typing typing-1"></div>
@@ -54,26 +89,8 @@ const ChatMessages = () => {
       </div>
     </p>
   </div> */}
-  {/* <p className="white-chat sent">Can you make it?</p> */}
-  <div className="grey-chat">
-    <p className="typing-dots">
-      <div className="typing typing-1"></div>
-      <div className="typing typing-2"></div>
-      <div className="typing typing-3"></div>
-    </p>
-  </div>
-  <p className="grey-chat received">
-    Yes, other Azure AI services also support customer managed keys.
-    Azure AI services offer multiple options for customers to manage
-    keys, such as using Azure Key Vault, customer-managed keys in
-    Azure Key Vault or customer-managed keys through Azure Storage
-    service. This helps customers ensure that their data is secure
-    and access to their services is controlled.
-  </p>
-  <p className="white-chat sent">
-  Does Azure OpenAI support customer managed keys?
-  </p>
-  {/* <label for="op30m" className="option received">
+      {/* <p className="white-chat sent">Can you make it?</p> */}
+      {/* <label for="op30m" className="option received">
 <input type="radio" name="option" id="op30m" />
 <label for="op30m">30 minute walk </label>
 <p>$29</p>
@@ -82,8 +99,9 @@ const ChatMessages = () => {
 <input type="radio" name="option" id="op1h" />
 <label for="op1h">1 hour walk </label>
 <p>$49</p>
-</label> */}</div>
-  )
-}
+</label> */}
+    </div>
+  );
+};
 
-export default ChatMessages
+export default ChatMessages;
